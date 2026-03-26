f41fb63f00b3eb74a7d82838ed7be3d89e80f78e
Thomas Mann <thomas.mann@framefield.com>
2026-03-27 00:43:21 +0100

    update: [FilterPoints] now returns last point for index -1

--- Operators/Lib/Assets/shaders/points/generate/FilterPoints.hlsl
+++ Operators/Lib/Assets/shaders/points/generate/FilterPoints.hlsl
@@ -2,41 +2,40 @@
 #include "shared/point.hlsl"
 #include "shared/quat-functions.hlsl"
 
-cbuffer Params : register(b0)
+cbuffer FloatParams : register(b0)
 {
-    float StartIndex;
     float Scatter;
-    float Seed;
     float StepSize;
+}
+
+cbuffer IntParams : register(b1)
+{
+    int SourceCount;
+    int ResultCount;
+    int StartIndex;
+    int Seed;
 }
 
 StructuredBuffer<Point> SourcePoints : t0;
 RWStructuredBuffer<Point> ResultPoints : u0;
 
-int imod(int x, int y)
+inline int imod2(int val, int repeat) 
 {
-    return x >= 0 ? x % y
-                  : y + ((x + 1) % y) - 1; // there are probably easier ways to do this
+    int x = val % repeat;
+    return x + (x < 0) * repeat;
 }
 
 [numthreads(64, 1, 1)] void main(uint3 i : SV_DispatchThreadID)
 {
-    uint sourceCount, stride;
-    SourcePoints.GetDimensions(sourceCount, stride);
+    int idx= i.x;
 
-    uint resultCount, stride2;
-    ResultPoints.GetDimensions(resultCount, stride2);
-
-    if (i.x >= resultCount)
-    {
+    if (idx >= ResultCount)
         return;
-    }
 
-    uint scatterOffset = Scatter > 0.001
-                             ? (float)sourceCount * Scatter * hash11(i.x + Seed % 4321 + int(StartIndex + 0.5) % 1234)
-                             : 0;
+    int scatterOffset = Scatter > 0.001
+                             ? (float)SourceCount * Scatter * hash11u(idx + Seed % 54321 + StartIndex)
+                             : 0 ;
 
-    // uint index = imod((int)StartIndex + (i.x * StepSize) + scatterOffset + 0.1,  sourceCount);
-    int index = imod(int(StartIndex + 0.5) + (i.x * StepSize) + scatterOffset, sourceCount);
+    int index = imod2(StartIndex + (int)((i.x * StepSize) + scatterOffset), SourceCount);
     ResultPoints[i.x] = SourcePoints[index];
 }
